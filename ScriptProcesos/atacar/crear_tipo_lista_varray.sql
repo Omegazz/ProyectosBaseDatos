b9c29cedffcd703b32342b6a8642da74c166ff01
Diego Salas <diegosalas.arce@gmail.com>
2014-01-31 11:27:58 -0600

    Create crear_tipo_lista_varray.sql
    Creacion del script de creacion de tipo de dato definido por el usuario

---
+++ ScriptProcesos/atacar/crear_tipo_lista_varray.sql
@@ -0,0 +1,8 @@
+--Autor: Diego Salas
+--Resumen: Crea un tipo de dato definido por el usuario de tipo
+-- varray numerico de 20 espacios maximo.
+--Fecha creacion: 20/08/2013
+--Fecha modificacion:29/01/2014
+--=================================================
+create or replace type lista is varray(20) of number;
+/
